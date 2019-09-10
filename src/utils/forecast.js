c1aa272de307c56a32d777c0da31296352a737d7
Georg Popp <Georg.Popp@outlook.com>
2019-09-10 12:26:43 +0200

    Add daily high and low temperature to forecast.js

--- src/utils/forecast.js
+++ src/utils/forecast.js
@@ -1,27 +1,28 @@
 const request = require('request');
 
 const forecast = (latitude, longitude, callback) => {
-    const url = `https://api.darksky.net/forecast/ca6bfc7f072fe5d8a9c69c331e4e910d/${longitude},${latitude}?units=ca`;
+	const url = `https://api.darksky.net/forecast/ca6bfc7f072fe5d8a9c69c331e4e910d/${longitude},${latitude}?units=ca`;
 
-    request({ url, json: true }, (err, { body }) => {
-        if (err) {
-            callback('Unable to connect to waether service!', undefined);
-        } else if (body.error) {
-            callback('Unable to find location', undefined);
-        } else {
-            const currentWeatherInfo = body.currently;
-            const dailySummary = body.daily.data[0].summary;
+	request({ url, json: true }, (err, { body }) => {
+		if (err) {
+			callback('Unable to connect to waether service!', undefined);
+		} else if (body.error) {
+			callback('Unable to find location', undefined);
+		} else {
+			const currentWeatherInfo = body.currently;
+			const dailySummary = body.daily.data[0].summary;
 
-            const { temperature, precipProbability } = currentWeatherInfo;
+			const { temperatureHigh, temperatureLow } = body.daily.data[0];
+			const { temperature, precipProbability } = currentWeatherInfo;
 
-            callback(
-                undefined,
-                dailySummary +
-                    ` It is currenty ${temperature} degrees out.` +
-                    ` There is a ${precipProbability}% chance of rain.`
-            );
-        }
-    });
+			callback(
+				undefined,
+				dailySummary +
+					` It is currenty ${temperature}°C out.` +
+					` This high today is ${temperatureHigh}°C with a low of ${temperatureLow}°C. There is a ${precipProbability}% chance of rain.`
+			);
+		}
+	});
 };
 
 module.exports = forecast;
